b8bf3c71156b5877f4b3e8f106e9629f53a8c64c
xuxu <clarkxdy@gmail.com>
2022-05-08 23:20:53 +0800

    First upload

---
+++ SDF_gen.cs
@@ -0,0 +1,293 @@
+using UnityEngine;
+using UnityEditor;
+
+/// Ref:
+/// 1. http://www.codersnotes.com/notes/signed-distance-fields/
+/// 2. https://shaderfun.com
+/// 3. https://zhuanlan.zhihu.com/p/337944099
+
+public class SDF_gen : MonoBehaviour
+{   
+    public struct Pixel
+    {
+        public float distance;
+        public bool edge;
+    }
+
+    public int targetSize = 64;
+    int m_x_dims;
+    int m_y_dims;
+    Pixel[] m_pixels;
+    public Texture2D[] Source;
+    int samples;
+    public int sampleTimes = 100;
+    public string outputName;
+
+    public void Generate()
+    {
+        SaveTexture(Generator(Source));
+    }
+    
+    void LoadFromTexture(Texture2D texture)
+    {
+        Color[] texpixels = texture.GetPixels();
+        m_x_dims = texture.width;
+        m_y_dims = texture.height;
+        m_pixels = new Pixel[m_x_dims * m_y_dims];
+        for (int i = 0; i < m_pixels.Length; i++)
+        {
+            if (texpixels[i].r > 0.5f)
+                m_pixels[i].distance = -99999f;
+            else
+                m_pixels[i].distance = 99999f;
+        }
+    }
+
+    void BuildSweepGrids(out float[] outside_grid, out float[] inside_grid)
+    {
+        outside_grid = new float[m_pixels.Length];
+        inside_grid = new float[m_pixels.Length];
+        for (int i = 0; i < m_pixels.Length; i++)
+        {
+            if (m_pixels[i].distance < 0)
+            {
+                //inside pixel. outer distance is set to 0, inner distance
+                //is preserved (albeit negated to make it positive)
+                outside_grid[i] = 0f;
+                inside_grid[i] = -m_pixels[i].distance;
+            }
+            else
+            {
+                //outside pixel. inner distance is set to 0,
+                //outer distance is preserved
+                inside_grid[i] = 0f;
+                outside_grid[i] = m_pixels[i].distance;
+            }
+        }
+    }
+
+    Texture2D Generator(Texture2D[] source)
+    {
+        Texture2D dest = new Texture2D(targetSize, targetSize);
+        if(source.Length== 0) return dest;
+
+        var pixels_out = new Color[targetSize*targetSize];
+
+        var dists_temp = new float[source.Length, targetSize*targetSize];
+        
+        /// Calculate all the textures and store them for further interpolation
+        for(int count = 0; count < source.Length; count++)
+        {
+            samples = (int)sampleTimes/source.Length;
+            LoadFromTexture(source[count]);
+            ClearAndMarkNoneEdgePixels();
+            float[] outside_grid, inside_grid;
+            BuildSweepGrids(out outside_grid, out inside_grid);
+
+            //run the 8PSSEDT sweep on each grid
+            SweepGrid(outside_grid);
+            SweepGrid(inside_grid);
+            
+            int scaleX = source[count].width / targetSize;
+            int scaleY = source[count].height / targetSize;
+
+            for (int y = 0; y < targetSize; y++)
+            {
+                for (int x = 0; x < targetSize; x++)
+                {
+                    int i = y * targetSize + x;
+                    float dist1 = inside_grid[y * scaleY * m_x_dims + x * scaleX];
+                    float dist2 = outside_grid[y * scaleY * m_x_dims + x * scaleX];
+                    var dist = Mathf.Clamp(128f + (dist1 - dist2), 0f, 255f);
+                    dist = Mathf.InverseLerp(0f, 255f, dist);
+                    dists_temp[count, i] = dist;
+                    
+                    // One SDF without interpolation
+                    if (source.Length == 1) pixels_out[i] = new Color(dist, dist, dist, 1f);
+                }
+            }
+        }
+
+        /// Interpolate using SdF_Interpolation function
+        float[] dists_intrp = new float[targetSize*targetSize];
+        if (source.Length >= 2)
+        {
+            float gap = 1f/(source.Length - 1);
+            for(int count = 0; count < source.Length - 1; count++)
+            {
+                for (int y = 0; y < targetSize; y++)
+                {
+                    for (int x = 0; x < targetSize; x++)
+                    {
+                        int i = y * targetSize + x;
+                        dists_intrp[i] += (float)SDF_Interpolation(dists_temp[count, i], dists_temp[count+1, i], new Vector2(1f - (count+1)*gap , 1f - count*gap));
+                    }
+                }
+            }
+
+            for (int y = 0; y < targetSize; y++)
+            {
+                for (int x = 0; x < targetSize; x++)
+                {
+                    int i = y * targetSize + x;
+                    pixels_out[i] = new Color(dists_intrp[i], dists_intrp[i], dists_intrp[i], 1f);
+                }
+            }
+        }
+        
+        dest.SetPixels(pixels_out);
+        dest.Apply();
+
+        return dest;
+    }
+
+    //compare a pixel for the sweep, and updates it with a new distance if necessary
+    void Compare(float[] grid, int x, int y, int xoffset, int yoffset)
+    {
+        //calculate the location of the other pixel, and bail if in valid
+        int otherx = x + xoffset;
+        int othery = y + yoffset;
+        if (otherx < 0 || othery < 0 || otherx >= m_x_dims || othery >= m_y_dims)
+            return;
+
+        //read the distance values stored in both this and the other pixel
+        float curr_dist = grid[y * m_x_dims + x];
+        float other_dist = grid[othery * m_x_dims + otherx];
+
+        //calculate a potential new distance, using the one stored in the other pixel,
+        //PLUS the distance to the other pixel
+        float new_dist = other_dist + Mathf.Sqrt(xoffset * xoffset + yoffset * yoffset);
+
+        //if the potential new distance is better than our current one, update!
+        if (new_dist < curr_dist)
+            grid[y * m_x_dims + x] = new_dist;
+    }
+
+    void SweepGrid(float[] grid)
+    {
+        // Pass 0
+        //loop over rows from top to bottom
+        for (int y = 0; y < m_y_dims; y++)
+        {
+            //loop over pixels from left to right
+            for (int x = 0; x < m_x_dims; x++)
+            {
+                Compare(grid, x, y, -1, 0);
+                Compare(grid, x, y, 0, -1);
+                Compare(grid, x, y, -1, -1);
+                Compare(grid, x, y, 1, -1);
+            }
+
+            //loop over pixels from right to left
+            for (int x = m_x_dims - 1; x >= 0; x--)
+            {
+                Compare(grid, x, y, 1, 0);
+            }
+        }
+
+        // Pass 1
+        //loop over rows from bottom to top
+        for (int y = m_y_dims - 1; y >= 0; y--)
+        {
+            //loop over pixels from right to left
+            for (int x = m_x_dims - 1; x >= 0; x--)
+            {
+                Compare(grid, x, y, 1, 0);
+                Compare(grid, x, y, 0, 1);
+                Compare(grid, x, y, -1, 1);
+                Compare(grid, x, y, 1, 1);
+            }
+
+            //loop over pixels from left to right
+            for (int x = 0; x < m_x_dims; x++)
+            {
+                Compare(grid, x, y, -1, 0);
+            }
+        }
+    }
+
+
+    Pixel GetPixel(int x, int y)
+    {
+        return m_pixels[y * m_x_dims + x];
+    }
+    void SetPixel(int x, int y, Pixel p)
+    {
+        m_pixels[y * m_x_dims + x] = p;
+    }
+
+    bool IsOuterPixel(int pix_x, int pix_y)
+    {
+        if (pix_x < 0 || pix_y < 0 || pix_x >= m_x_dims || pix_y >= m_y_dims)
+            return true;
+        else
+            return GetPixel(pix_x, pix_y).distance >= 0;
+    }
+
+    bool IsEdgePixel(int pix_x, int pix_y)
+    {
+        bool is_outer = IsOuterPixel(pix_x, pix_y);
+        if (is_outer != IsOuterPixel(pix_x - 1, pix_y - 1)) return true; //[-1,-1]
+        if (is_outer != IsOuterPixel(pix_x, pix_y - 1)) return true;     //[ 0,-1]
+        if (is_outer != IsOuterPixel(pix_x + 1, pix_y - 1)) return true; //[+1,-1]
+        if (is_outer != IsOuterPixel(pix_x - 1, pix_y)) return true;     //[-1, 0]
+        if (is_outer != IsOuterPixel(pix_x + 1, pix_y)) return true;     //[+1, 0]
+        if (is_outer != IsOuterPixel(pix_x - 1, pix_y + 1)) return true; //[-1,+1]
+        if (is_outer != IsOuterPixel(pix_x, pix_y + 1)) return true;     //[ 0,+1]
+        if (is_outer != IsOuterPixel(pix_x + 1, pix_y + 1)) return true; //[+1,+1]
+        return false;
+    }
+
+    public void ClearAndMarkNoneEdgePixels()
+    {
+        for (int y = 0; y < m_y_dims; y++)
+        {
+            for (int x = 0; x < m_y_dims; x++)
+            {
+                Pixel pix = GetPixel(x, y);
+                pix.edge = IsEdgePixel(x, y); //mark edge pixels
+                if (!pix.edge)
+                    pix.distance = pix.distance > 0 ? 99999f : -99999f;
+                SetPixel(x, y, pix);
+            }
+        }
+    }
+
+    float SDF_Interpolation(float sdfA,float sdfB, Vector2 sdfEdge)
+    {
+        // SDF interpolattion and define threshold
+        if (sdfA < .5f && sdfB < .5f)
+        {
+            return 0f;
+        }
+        if (sdfA > .5f && sdfB > .5f)
+        {
+            if(sdfEdge.y == 1f) return 1f;
+            return 0f;
+        }
+
+        float result = 0;
+        for (int i = 0; i < samples; i++)
+        {
+            float t = (float)i/samples; // (float) is needed
+            result += Mathf.Lerp(sdfA, sdfB, t) < .5f? sdfEdge.x : sdfEdge.y;
+        }
+
+        return result / samples;
+    }
+    void SaveTexture(Texture2D texture)
+    {
+        byte[] bytes = texture.EncodeToPNG();
+        var dirPath = Application.dataPath.Replace("Assets", "") + AssetDatabase.GetAssetPath(Source[0]).Replace(Source[0].name, outputName);
+        if (outputName == null || outputName == "")
+        {
+            //if the output name is blank, fill in the name based on the original texture
+            dirPath = Application.dataPath.Replace("Assets", "") + AssetDatabase.GetAssetPath(Source[0]).Replace(Source[0].name, Source[0].name + "_SDFIntrp");
+        }
+        System.IO.File.WriteAllBytes(dirPath, bytes);
+        Debug.Log(bytes.Length / 1024 + "Kb was saved as: " + dirPath);
+        #if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
+        #endif
+    }
+}
